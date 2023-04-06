0d371eaf8d0eb4bc386696aa0741b7c38af53c60
Armanissakov <arman.isakov.96@mail.ru>
2023-04-06 17:33:24 +0600

    hw04 is done

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
+
+
     public static void main(String[] args) {
-        MyList list = new MyList();
+/*        MyList list = new MyList();
         list.add(1);
         list.add(2);
         list.add(3);
@@ -9,7 +15,21 @@ public class Main {
         list.showList();
         list.revert();
         System.out.println();
-        list.showList();
+        list.showList();*/
+        final RedBlackTree tree = new RedBlackTree();
+        tree.add(1);
+        tree.add(3);
+        tree.add(4);
+        tree.add(5);
+        tree.add(6);
+        tree.add(7);
+        System.out.println(tree.getRoot());
+        System.out.println(tree.getRoot().getLeftChild());
+        System.out.println(tree.getRoot().getRightChild());
+        System.out.println(tree.getRoot().getRightChild().getLeftChild());
+        /*                             5:BLACK
+        *               3:BLACK                     7:BLACK
+        *        1:BLACK       4:BLACK         6:RED       NULL */
     }
 
 }
